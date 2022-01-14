590962b0cc593acd78c176d95e85bbc2cad42b07
Joris Kamminga <joris.kamminga@gmail.com>
2022-01-14 20:15:18 +0100

    undid some previous changes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,6 @@ import "./styles/index.scss";
 function App() {
   return (
     <>
-      <div className="container">
-        <h1 className="title">
-          Hey,
-          <br /> my name is Joris
-        </h1>
-      </div>
-      <div className="container dark">
-        <h1 className="title">
-          Hey,
-          <br /> my name is Joris
-        </h1>
-      </div>
     </>
   );
 }
